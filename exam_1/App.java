8d6090e81e397e4e64f80a4364fba4bf60689d47
herreraten <112837135+herreraten@users.noreply.github.com>
2022-09-05 10:12:39 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+
+        Student Me = new Student();
+
+        Me.surname = "HERRERA";
+        Me.firstName = "TENTEN";
+        Me.middleInitial = "C";
+        Me.birthdate = 11/06/02;
+        Me.studentNumber = 2022109794;
+        Me.email ="herreracc@students.national-u.edu.ph";
+        Me.iAmAwesome = true;
+        Me.sayMystudentNumber();
+        Me.sayMyemail();
+        Me.MeDialogue();
+
+        
+    }
+}
