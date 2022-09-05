90bc15b6cc74fdd55cb33760ed6c75eca56271d3
herreraten <112837135+herreraten@users.noreply.github.com>
2022-09-05 10:13:01 +0800

    Add files via upload

---
+++ exam_1/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+
+    String surname;
+    String firstName;
+    String middleInitial;
+    int birthdate;
+    int studentNumber;
+    String email;
+    boolean iAmAwesome;
+
+    public void sayMystudentNumber() {
+        System.out.println(studentNumber);
+    }
+
+        public void sayMyemail() {
+            System.out.println(email);
+        }
+
+            public void MeDialogue() {
+                System.out.println("This computer will say that it's " + iAmAwesome + " that I am awesome");
+            
+    
+    }
+}
